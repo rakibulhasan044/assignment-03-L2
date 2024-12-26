3b7c232956ec06b3b6db7b9dc097420f3929e725
rakibulhasan044 <rhrakib044@gmail.com>
2024-12-26 16:54:31 +0600

    user model added

--- src/app/module/user/user.validation.ts
+++ src/app/module/user/user.validation.ts
@@ -2,10 +2,9 @@ import { z } from 'zod';
 
 const createUserValidationSchema = z.object({
   body: z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-    role: z.string(),
+    name: z.string({message: "name is required"}),
+    email: z.string().email({message: "Invalid email address !!"}),
+    password: z.string().min(6, {message: "Password must be minimum 6 characters !!"}),
   }),
 });
 
